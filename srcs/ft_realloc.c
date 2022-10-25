f217c3edcfc6c81da110ee7ccd6d8fa4ecfb3291
Mederis Frambourt <mederis.frambourt@gmail.com>
2022-10-25 16:32:21 +0200

    Realloc now changes category if size is too different

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -1,6 +1,36 @@
 #include <libft.h>
 #include "./ft_malloc.h"
 
+int get_size_category(size_t size)
+{
+	if (size <= TINY_FT_MALLOC_MAX_SIZE)
+		return 0;
+	else if (size <= SMALL_FT_MALLOC_MAX_SIZE)
+		return 1;
+	else
+		return 2;
+}
+
+/**
+ * @brief Checks if the given (block / page) is in the same category as the new size
+ * (eg. malloc of 100 then realloc of 2000 is not in the same category, so change category)
+ *
+ * @param data the data for the given (block / page)
+ * @param new_size the target size
+ * @return int 0/1 whether it's in the same category or not
+ */
+int is_same_size_category(t_find_block_data data, size_t new_size)
+{
+	if (!data.block || !data.page)
+		return (0);
+
+	size_t original_size = data.block->real_size - sizeof(t_block);
+	int original_category = get_size_category(original_size);
+	int new_category = get_size_category(new_size);
+
+	return (original_category == new_category);
+}
+
 void *ft_realloc(void *ptr, size_t size)
 {
 	if (ptr == NULL)
@@ -20,6 +50,17 @@ void *ft_realloc(void *ptr, size_t size)
 
 	if (data.block == NULL)
 		return NULL;
+
+	if (!is_same_size_category(data, size))
+	{
+		void *new_ptr = ft_malloc(size);
+		if (new_ptr == NULL)
+			return NULL;
+
+		ft_memcpy(new_ptr, ptr, data.block->real_size - sizeof(t_block));
+		ft_free(ptr);
+		return new_ptr;
+	}
 
 	t_block *block = data.block;
 	t_page *page = data.page;
